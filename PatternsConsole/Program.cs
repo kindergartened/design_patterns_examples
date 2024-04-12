0c60fac39498da7eccc4ec9411644b628883a883
Krav1tar <gleb.vinogradovv@bk.ru>
2024-04-12 15:32:58 +0300

    Decorator+Console

--- PatternsConsole/Program.cs
+++ PatternsConsole/Program.cs
@@ -1,8 +1,24 @@
-﻿namespace PatternsConsole;
+﻿using SimpleDesignPatterns;
+namespace PatternsConsole;
 
 class Program
 {
     static void Main()
     {
+        /*  _____  ______ _____ ____  _____         _______ ____  _____  
+            |  __ \|  ____/ ____/ __ \|  __ \     /\|__   __/ __ \|  __ \ 
+            | |  | | |__ | |   | |  | | |__) |   /  \  | | | |  | | |__) |
+            | |  | |  __|| |   | |  | |  _  /   / /\ \ | | | |  | |  _  / 
+            | |__| | |___| |___| |__| | | \ \  / ____ \| | | |__| | | \ \ 
+            |_____/|______\_____\____/|_|  \_\/_/    \_\_|  \____/|_|  \_\                                                
+         */
+        Beverage beverage = new Espresso();
+        Console.WriteLine(beverage.GetDescription() + ": " + beverage.GetCost());
+
+        Beverage beverageWithMilk = new MilkDecorator(beverage);
+        Console.WriteLine(beverageWithMilk.GetDescription() + ": " + beverageWithMilk.GetCost());
+
+        Beverage beverageWithMilkAndSugar = new SugarDecorator(beverageWithMilk);
+        Console.WriteLine(beverageWithMilkAndSugar.GetDescription() + ": " + beverageWithMilkAndSugar.GetCost());
     }
 }
--- SimpleDesignPatterns/Decorator/Decorator.cs
+++ SimpleDesignPatterns/Decorator/Decorator.cs
@@ -1,5 +1,76 @@
 ﻿namespace SimpleDesignPatterns;
 
-public class Decorator
+using System;
+/// <summary>
+/// Абстрактный класс, представляющий собой напиток
+/// </summary>
+public abstract class Beverage
 {
+    public string? Description { get; set; }
+    public double Cost { get; set; }
+
+    public virtual string GetDescription()
+    {
+        return Description;
+    }
+
+    public virtual double GetCost()
+    {
+        return Cost;
+    }
+}
+/// <summary>
+/// Конкретный класс, наследуется от класса Напиток, представляет собой Эспрессо
+/// </summary>
+public class Espresso : Beverage
+{
+    public Espresso()
+    {
+        Description = "Эспрессо";
+        Cost = 49;
+    }
+}
+/// <summary>
+/// Абстрактный декоратор, также наследуется от класса Напиток
+/// </summary>
+public class BeverageDecorator : Beverage
+{
+    private Beverage _beverage;
+
+    public BeverageDecorator(Beverage beverage)
+    {
+        _beverage = beverage;
+    }
+
+    public override string GetDescription()
+    {
+        return _beverage.GetDescription() + ", " + Description;
+    }
+
+    public override double GetCost()
+    {
+        return _beverage.GetCost() + Cost;
+    }
+}
+/// <summary>
+/// Конкретный декоратор, наследуется от декоратора, добавляет к напитку молоко
+/// </summary>
+public class MilkDecorator : BeverageDecorator
+{
+    public MilkDecorator(Beverage beverage) : base(beverage)
+    {
+        Description += "с молоком";
+        Cost += 10;
+    }
+}
+/// <summary>
+/// Еще один конкретный декоратор, добавляет к напитку сахар
+/// </summary>
+public class SugarDecorator : BeverageDecorator
+{
+    public SugarDecorator(Beverage beverage) : base(beverage)
+    {
+        Description += "с сахаром";
+        Cost += 5;
+    }
 }
